6b6121f512eef6e21b3f926f579460954cd096b3
Mahmoud Ahmed <mod664h@gmail.com>
2022-05-15 16:02:31 +0200

    log-in function

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -1,7 +1,8 @@
 var querie =require("../database/querie");
 var dbconnection =require('../database/connection');
+var userModel =require("../model/userModel");
 const { set } = require("express/lib/response");
-
+const { json } = require("body-parser");
 
 
 exports.userSingUp = async (req,res)=>{
@@ -18,10 +19,41 @@ exports.userSingUp = async (req,res)=>{
     values=[userName,userEmail,userPassword,userPhone];
     var usersSignupQuery=querie.queryList.USERS_SIGNUP_QUERY;
     await dbconnection.dbQuery(usersSignupQuery ,values);
-    return res.status(201).send("save store")
+    return res.status(201).send("sing up is done")
     } catch (err) {
         console.log("Error : "+ err)
-        return res.status(500).send({error:'failed to save store'})
+        return res.status(500).send({error:'sing up failed '})
     }
     
+}
+exports.userLogeIn = async (req,res)=>{
+    try {
+        var userEmail=req.body.email;
+        var userPassword=req.body.password;
+        var authorizationQuery=querie.queryList.AUTHORIZATION;
+        var userData = userModel.userData;
+        var userDBValue = await dbconnection.dbQuery(authorizationQuery,[userEmail ,userPassword]);
+        userDB=JSON.stringify(userDBValue.rows[0]);
+        if((JSON.stringify(userDBValue.rows))==="[]"){
+            return res.status(500).send({error:'Incorrect Email or Password'})
+        }
+        else
+        {
+            var userDataValue = new userData(
+            JSON.parse(userDB).user_id,
+            JSON.parse(userDB).user_name,
+            JSON.parse(userDB).user_email,
+            JSON.parse(userDB).user_password,
+            JSON.parse(userDB).user_phone);
+            return res.status(200).json({
+                user_id :userDataValue.user_id ,
+                user_name :userDataValue.user_name,
+            });
+        }
+
+    } catch (err) {
+        console.log("Error : "+err)
+        return res.status(500).send({error:'failed log in'})
+    }
+
 }
---
+++ server/model/userModel.js
@@ -0,0 +1,12 @@
+exports.userData =class userData{
+
+    constructor(user_id,user_name,user_email,user_password,user_phone){
+        this.user_id=user_id;
+        this.user_name=user_name;
+        this.user_email=user_email;
+        this.user_password=user_password;
+        this.user_phone=user_phone;
+
+    }
+
+}
