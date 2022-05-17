a746a44dd25cf394bdcd8a63de4a6d054c0da13b
Mahmoud Ahmed <mod664h@gmail.com>
2022-05-17 16:59:32 +0200

    profile

--- server/controller/usersController.js
+++ server/controller/usersController.js
@@ -1,59 +1,65 @@
-var querie =require("../database/querie");
-var dbconnection =require('../database/connection');
-var userModel =require("../model/userModel");
+var querie = require("../database/querie");
+var dbconnection = require("../database/connection");
+var userModel = require("../model/userModel");
+var clubModel = require("../model/clubModel");
+var bookingModel = require("../model/bookingModel");
 const { set } = require("express/lib/response");
 const { json } = require("body-parser");
 
-
-exports.userSingUp = async (req,res)=>{
-    console.log(req.body);
-    try {
-    var userName=req.body.firstName + " "+req.body.surName;
-    var userEmail=req.body.email;
-    var userPassword=req.body.password;
-    var userPhone=req.body.phone;
-    if(!userName|| !userEmail || !userPassword){
-        return res.status(500).send('user Name , user Email and user password are required , cannot empty')
+exports.userSingUp = async (req, res) => {
+  console.log(req.body);
+  try {
+    var userName = req.body.firstName + " " + req.body.surName;
+    var userEmail = req.body.email;
+    var userPassword = req.body.password;
+    var userPhone = req.body.phone;
+    if (!userName || !userEmail || !userPassword) {
+      return res
+        .status(500)
+        .send(
+          "user Name , user Email and user password are required , cannot empty"
+        );
     }
 
-    values=[userName,userEmail,userPassword,userPhone];
-    var usersSignupQuery=querie.queryList.USERS_SIGNUP_QUERY;
-    await dbconnection.dbQuery(usersSignupQuery ,values);
-    return res.status(201).send("sing up is done")
-    } catch (err) {
-        console.log("Error : "+ err)
-        return res.status(500).send({error:'sing up failed '})
-    }
-    
-}
-exports.userLogeIn = async (req,res)=>{
-    try {
-        var userEmail=req.body.email;
-        var userPassword=req.body.password;
-        var authorizationQuery=querie.queryList.AUTHORIZATION;
-        var userData = userModel.userData;
-        var userDBValue = await dbconnection.dbQuery(authorizationQuery,[userEmail ,userPassword]);
-        userDB=JSON.stringify(userDBValue.rows[0]);
-        if((JSON.stringify(userDBValue.rows))==="[]"){
-            return res.status(500).send({error:'Incorrect Email or Password'})
-        }
-        else
-        {
-            var userDataValue = new userData(
-            JSON.parse(userDB).user_id,
-            JSON.parse(userDB).user_name,
-            JSON.parse(userDB).user_email,
-            JSON.parse(userDB).user_password,
-            JSON.parse(userDB).user_phone);
-            return res.status(200).json({
-                user_id :userDataValue.user_id ,
-                user_name :userDataValue.user_name,
-            });
-        }
+    values = [userName, userEmail, userPassword, userPhone];
+    var usersSignupQuery = querie.queryList.USERS_SIGNUP_QUERY;
+    await dbconnection.dbQuery(usersSignupQuery, values);
+    return res.status(201).send("sing up is done");
+  } catch (err) {
+    console.log("Error : " + err);
+    return res.status(500).send({ error: "sing up failed " });
+  }
+};
+exports.userLogeIn = async (req, res) => {
+  try {
+    var userEmail = req.body.email;
+    var userPassword = req.body.password;
+    var authorizationQuery = querie.queryList.AUTHORIZATION;
+    var userCaetItemQuery = querie.queryList.USER_CART_ITEMS;
+    var userDBValue = await dbconnection.dbQuery(authorizationQuery, [
+      userEmail,
+      userPassword,
+    ]);
 
-    } catch (err) {
-        console.log("Error : "+err)
-        return res.status(500).send({error:'failed log in'})
+    if (userDBValue.rowCount === 0) {
+      return res.status(500).json({ error: "Incorrect Email or Password" });
+    } else {
+      var bookingDBValue = await dbconnection.dbQuery(userCaetItemQuery, [
+        userDBValue.rows[0].user_id,
+      ]);
+      bookingDBValue.rows.forEach(element => {
+        delete element.admins_id;
+        delete element.user_id;
+      });
+      console.log(bookingDBValue.rows);
+      return res.status(200).json({
+        user_id: userDBValue.rows[0].user_id,
+        user_name: userDBValue.rows[0].user_name,
+        cardItems: bookingDBValue.rows,
+      });
     }
-
-}
+  } catch (err) {
+    console.log("Error : " + err);
+    return res.status(500).send({ error: "failed log in" });
+  }
+};
--- server/route/usersRoute.js
+++ server/route/usersRoute.js
@@ -3,6 +3,6 @@ const router=express.Router();
 var usersCtrl = require('../controller/usersController');
 
 router.post("/sign-up" ,usersCtrl.userSingUp);
-router.put("/log-in" ,usersCtrl.userLogeIn);
+router.post("/log-in" ,usersCtrl.userLogeIn);
 
 module.exports =router;
