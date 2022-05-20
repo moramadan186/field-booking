df825322de9a97c38193ad227ec9fb69cab68a3f
Mahmoud Ahmed <mod664h@gmail.com>
2022-05-20 22:18:12 +0200

    ddl script and cart item

--- server/scripts/ddl.sql
+++ server/scripts/ddl.sql
@@ -7,38 +7,33 @@
 CREATE TABLE booking.admins (
 	admins_id serial4 NOT NULL,
 	admins_ssd_id varchar(100) NOT NULL,
-	admins_name varchar(30) NOT NULL,
+	admins_firstname varchar(30) NOT NULL,
+	admins_surname varchar(30) NOT NULL,
 	admins_email varchar(50) NOT NULL,
 	admins_password text NOT NULL,
 	admins_phone text NOT NULL,
 	CONSTRAINT admins_email_pkey UNIQUE (admins_email),
-	CONSTRAINT admins_pkey PRIMARY KEY (admins_id)
+	CONSTRAINT admins_pkey PRIMARY KEY (admins_id),
+	CONSTRAINT ssd_pkey UNIQUE (admins_ssd_id)
 );
 
-
--- booking.booked definition
+-- booking.users definition
 
 -- Drop table
 
--- DROP TABLE booking.booked;
+-- DROP TABLE booking.users;
 
-CREATE TABLE booking.booked (
-	booked_id serial4 NOT NULL,
-	user_id int4 NOT NULL,
-	admins_id int4 NOT NULL,
-	booked_data_work date NOT NULL,
-	booked_time_start time NOT NULL,
-	booked_time_end time NOT NULL,
-	state bool NULL,
-	CONSTRAINT booked_pkey PRIMARY KEY (booked_id)
+CREATE TABLE booking.users (
+	user_id serial4 NOT NULL,
+	user_firstname varchar(30) NOT NULL,
+	user_surname varchar(30) NOT NULL,
+	user_email varchar(100) NOT NULL,
+	user_password text NOT NULL,
+	user_phone text NULL,
+	user_profileimg text NULL,
+	CONSTRAINT email_user_pkey UNIQUE (user_email),
+	CONSTRAINT user_pkey PRIMARY KEY (user_id)
 );
-
-
--- booking.booked foreign keys
-
-ALTER TABLE booking.booked ADD CONSTRAINT fk_admins_id FOREIGN KEY (admins_id) REFERENCES booking.admins(admins_id);
-ALTER TABLE booking.booked ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES booking.users(user_id);
-
 -- booking.club definition
 
 -- Drop table
@@ -49,7 +44,6 @@ CREATE TABLE booking.club (
 	club_id serial4 NOT NULL,
 	admins_id int4 NOT NULL,
 	club_name varchar(30) NOT NULL,
-	club_date_work date NOT NULL,
 	club_time_work_from time NOT NULL,
 	club_time_work_to time NOT NULL,
 	club_location text NOT NULL,
@@ -64,6 +58,32 @@ CREATE TABLE booking.club (
 
 ALTER TABLE booking.club ADD CONSTRAINT fk_admins_id FOREIGN KEY (admins_id) REFERENCES booking.admins(admins_id);
 
+-- booking.booked definition
+
+-- Drop table
+
+-- DROP TABLE booking.booked;
+
+CREATE TABLE booking.booked (
+	booked_id serial4 NOT NULL,
+	user_id int4 NOT NULL,
+	admins_id int4 NOT NULL,
+	club_id int4 NOT NULL,
+	booked_data_work date NOT NULL,
+	booked_time_start time NOT NULL,
+	booked_time_end time NOT NULL,
+	state bool NULL,
+	CONSTRAINT booked_pkey PRIMARY KEY (booked_id)
+);
+
+
+-- booking.booked foreign keys
+
+ALTER TABLE booking.booked ADD CONSTRAINT fk_admins_id FOREIGN KEY (admins_id) REFERENCES booking.admins(admins_id);
+ALTER TABLE booking.booked ADD CONSTRAINT fk_club_id FOREIGN KEY (club_id) REFERENCES booking.club(club_id);
+ALTER TABLE booking.booked ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES booking.users(user_id);
+
+
 -- booking.payment definition
 
 -- Drop table
@@ -73,12 +93,9 @@ ALTER TABLE booking.club ADD CONSTRAINT fk_admins_id FOREIGN KEY (admins_id) REF
 CREATE TABLE booking.payment (
 	payment_id serial4 NOT NULL,
 	user_id int4 NOT NULL,
-	card_number int4 NOT NULL,
-	expiry_date date NULL,
-	cvc_number int4 NULL,
-	total_price int4 NULL,
 	booked_id int4 NOT NULL,
-	"name" varchar(50) NOT NULL,
+	phone_number int4 NOT NULL,
+	total_price int4 NULL,
 	CONSTRAINT payment_pkey PRIMARY KEY (payment_id)
 );
 
@@ -87,19 +104,3 @@ CREATE TABLE booking.payment (
 
 ALTER TABLE booking.payment ADD CONSTRAINT fk_booked_id FOREIGN KEY (booked_id) REFERENCES booking.booked(booked_id);
 ALTER TABLE booking.payment ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES booking.users(user_id);
-
--- booking.users definition
-
--- Drop table
-
--- DROP TABLE booking.users;
-
-CREATE TABLE booking.users (
-	user_id serial4 NOT NULL,
-	user_name varchar(30) NOT NULL,
-	user_email varchar(50) NOT NULL,
-	user_password text NOT NULL,
-	user_phone text NULL,
-	CONSTRAINT email_user_pkey UNIQUE (user_email),
-	CONSTRAINT user_pkey PRIMARY KEY (user_id)
-);
