be075d3e1a7a7ecc2905d140396b4e9d4e1d8935
Mahmoud Ahmed <mod664h@gmail.com>
2022-05-26 23:25:28 +0200

    Account data

--- server/route/usersRoute.js
+++ server/route/usersRoute.js
@@ -3,6 +3,6 @@ const router=express.Router();
 var usersCtrl = require('../controller/usersController');
 
 router.post("/sign-up" ,usersCtrl.userSingUp);
-router.post("/log-in" ,usersCtrl.userLogeIn);
+router.post("/sign-in" ,usersCtrl.userLogeIn);
 
 module.exports =router;
